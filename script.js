f6528b00ac64340c36b769a9993898419d0fe1b1
Breno L. Lopes <brernolopes@hotmail.com>
2022-08-14 11:03:12 -0300

    fix: corrigidos erros de lint e funcionalidade do projeto

--- script.js
+++ script.js
@@ -1,82 +1,59 @@
+const rndNumber = (min, max) => {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin)) + newMin;
+};
+
+const ball1 = document.getElementById('ball1');
+const ball2 = document.getElementById('ball2');
+const ball3 = document.getElementById('ball3');
+const ball4 = document.getElementById('ball4');
+const ball5 = document.getElementById('ball5');
+const ball6 = document.getElementById('ball6');
+
+const balls = [ball1, ball2, ball3, ball4, ball5, ball6];
+
+const colorElements = () => {
+  balls.forEach((element) => {
+    const color = element;
+    color.style.backgroundColor = `rgb(
+    ${rndNumber(0, 255)}, ${rndNumber(0, 255)}, ${rndNumber(0, 255)})`;
+  });
+};
+
+const colorRight = () => {
+  const pickACollor = rndNumber(1, 6);
+  const color = balls[pickACollor].style.backgroundColor;
+  console.log(color);
+  document.querySelector('#rgb-color').innerHTML = color;
+  const oldWinner = document.querySelector('.win');
+  if (oldWinner) oldWinner.classList.remove('win');
+  balls[pickACollor].classList.add('win');
+};
+
 function colorOnLoad() {
-  let cor1 = []
-  let cor2 = []
-  let cor3 = []
-  let cor4 = []
-  let cor5 = []
-  let cor6 = []
-  for (let index = 0; index < 18; index += 1) {
-    let randoColor = parseInt(Math.random() * 255)
-    if (cor1.length <= 2) {
-      cor1.push(randoColor)
-    } else if (cor2.length <= 2) {
-      cor2.push(randoColor)
-    } else if (cor3.length <= 2) {
-      cor3.push(randoColor)
-    } else if (cor4.length <= 2) {
-      cor4.push(randoColor)
-    } else if (cor5.length <= 2) {
-      cor5.push(randoColor)
-    } else if (cor6.length <= 2) {
-      cor6.push(randoColor)
-    }
-  }
-  document.getElementById('ball1').style.backgroundColor = 'rgb(' + cor1[0] + ',' + cor1[1] + ',' + cor1[2] + ')'
-  document.getElementById('ball2').style.backgroundColor = 'rgb(' + cor2[0] + ',' + cor2[1] + ',' + cor2[2] + ')'
-  document.getElementById('ball3').style.backgroundColor = 'rgb(' + cor3[0] + ',' + cor3[1] + ',' + cor3[2] + ')'
-  document.getElementById('ball4').style.backgroundColor = 'rgb(' + cor4[0] + ',' + cor4[1] + ',' + cor4[2] + ')'
-  document.getElementById('ball5').style.backgroundColor = 'rgb(' + cor5[0] + ',' + cor5[1] + ',' + cor5[2] + ')'
-  document.getElementById('ball6').style.backgroundColor = 'rgb(' + cor6[0] + ',' + cor6[1] + ',' + cor6[2] + ')'
+  colorElements();
+  colorRight();
 
-  let randoColorText = parseInt(Math.random() * 6) + 1
-  switch (randoColorText) {
-    case 1:
-      let cor1 = document.getElementById('ball1').style.backgroundColor
-      document.querySelector('#rgb-color').innerHTML = 'Adivinhe qual é essa cor: ' + cor1;
-      document.getElementById('ball1').classList.add('win')
-
-      break
-    case 2:
-      let cor2 = document.getElementById('ball2').style.backgroundColor
-      document.querySelector('#rgb-color').innerHTML = 'Adivinhe qual é essa cor: ' + cor2;
-      document.getElementById('ball2').classList.add('win')
-      break
-    case 3:
-      let cor3 = document.getElementById('ball3').style.backgroundColor
-      document.querySelector('#rgb-color').innerHTML = 'Adivinhe qual é essa cor: ' + cor3;
-      document.getElementById('ball3').classList.add('win')
-      break
-    case 4:
-      let cor4 = document.getElementById('ball4').style.backgroundColor
-      document.querySelector('#rgb-color').innerHTML = 'Adivinhe qual é essa cor: ' + cor4;
-      document.getElementById('ball4').classList.add('win')
-      break
-    case 5:
-      let cor5 = document.getElementById('ball5').style.backgroundColor
-      document.querySelector('#rgb-color').innerHTML = 'Adivinhe qual é essa cor: ' + cor5;
-      document.getElementById('ball5').classList.add('win')
-      break
-    case 6:
-      let cor6 = document.getElementById('ball6').style.backgroundColor
-      document.querySelector('#rgb-color').innerHTML = 'Adivinhe qual é essa cor: ' + cor6;
-      document.getElementById('ball6').classList.add('win')
-      break
-  }
-  let resposta = document.querySelector('#answer')
-  resposta.innerHTML = 'Escolha uma cor'
+  const rightAnswer = document.querySelector('#answer');
+  rightAnswer.innerHTML = 'Escolha uma cor';
 }
 
-function resposta(cor) {
-  let pontos = document.getElementById('score')
-  if (cor.classList.length == 2) {
-    document.getElementById('answer').innerHTML = 'Acertou!'
-    let scoreUp = parseInt(pontos.innerText) + 3
-    pontos.innerText = scoreUp
+function resposta({ target }) {
+  const pontos = document.getElementById('score');
+  if (target.classList.length === 2) {
+    document.getElementById('answer').innerHTML = 'Acertou!';
+    const scoreUp = parseInt(pontos.innerText, 10) + 3;
+    pontos.innerText = scoreUp;
+    colorOnLoad();
   } else {
-    document.getElementById('answer').innerHTML = 'Errou! Tente novamente!'
-    pontos.innerText = 0
+    document.getElementById('answer').innerHTML = 'Errou! Tente novamente!';
+    pontos.innerText = 0;
   }
 }
 
+balls.forEach((element) => {
+  element.addEventListener('click', resposta);
+});
 
-window.onload = colorOnLoad()
+window.onload = colorOnLoad;
