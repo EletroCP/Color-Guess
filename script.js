e667495c0c7af292b05e2571ae18d6aa9d1c9d94
Breno L. Lopes <brernolopes@hotmail.com>
2022-08-15 18:54:09 -0300

    feat: adicionado score board

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const playesPoints = [];
+const timeElapsed = Date.now();
+const today = new Date(timeElapsed);
+
 const rndNumber = (min, max) => {
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
@@ -30,18 +34,45 @@ const colorRight = () => {
   balls[pickACollor].classList.add('win');
 };
 
+const boardScreen = document.getElementById('scoreBoard');
+const containerPointes = document.getElementById('players');
+
+const addPlayer = (name, points, data) => {
+  const div = document.createElement('div');
+  div.classList.add('points');
+  const p = document.createElement('p');
+  const pData = document.createElement('p');
+
+  p.innerText = `${name}:  ${points}`;
+  pData.innerText = data;
+
+  div.appendChild(p);
+  div.appendChild(pData);
+  containerPointes.appendChild(div);
+};
+
+const loadScore = () => {
+  containerPointes.innerHTML = '';
+  playesPoints.forEach(({ name, points, data }) => {
+    addPlayer(name, points, data);
+  });
+};
+
 function colorOnLoad() {
+  loadScore();
   colorElements();
   colorRight();
 }
 
 const awnser = document.getElementById('answer');
+const possGameScore = document.getElementById('possGame');
 
 function resposta({ target }) {
   const pontos = document.getElementById('score');
   if (target.classList.length === 2) {
     awnser.innerHTML = 'Acertou!';
     const scoreUp = parseInt(pontos.innerText, 10) + 3;
+    possGameScore.innerText = scoreUp;
     pontos.innerText = scoreUp;
     colorOnLoad();
   } else {
@@ -50,6 +81,7 @@ function resposta({ target }) {
     balls.forEach((element) => {
       element.removeEventListener('click', resposta);
     });
+    boardScreen.style.display = 'flex';
   }
 }
 
@@ -67,5 +99,39 @@ resetButton.addEventListener('click', addAwnser);
 balls.forEach((element) => {
   element.addEventListener('click', resposta);
 });
+
+const playerInput = document.getElementById('playerName');
+
+const removeBoard = document.getElementById('removeBoard');
+
+playerInput.addEventListener('keyup', ({ target }) => {
+  if (target.value !== '') {
+    removeBoard.removeAttribute('disabled', 'disabled');
+    removeBoard.style.borderColor = 'whitesmoke';
+  } else {
+    removeBoard.style.borderColor = 'red';
+    removeBoard.setAttribute('disabled', 'disabled');
+  }
+});
+
+removeBoard.addEventListener('click', () => {
+  const objectBoard = {
+    name: playerInput.value,
+    points: possGameScore.innerText,
+    data: today.toLocaleDateString(),
+  };
+  playesPoints.push(objectBoard);
+  playesPoints.sort((a, b) => {
+    if (a.point < b.points) return 1;
+    if (a.points > b.points) return -1;
+    return 0;
+  });
+  boardScreen.style.display = 'none';
+  possGameScore.innerText = 0;
+  if (playesPoints.length >= 8) playesPoints.pop();
+  addAwnser();
+});
+
+// max 7 in board
 
 window.onload = colorOnLoad;
